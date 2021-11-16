c7605d2bec7ab2fccbd224a38a96b12e1eed362c
Henry Kam <h3nrykam@gmail.com>
2021-11-16 01:57:10 -0800

    add chart for duration

--- public/js/stats.js
+++ public/js/stats.js
@@ -1,35 +1,78 @@
-const ctx = document.getElementById('first').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
+const firstCtx = document.getElementById("first").getContext("2d");
+const secCtx = document.getElementById("second").getContext("2d");
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+function getDuration(data){
+    let sum = [];
+
+    data.forEach((workout) => {
+      const durationTotal = workout.exercise.reduce((sum, { duration }) => {
+       return sum + duration;
+      }, 0);
+      sum.push(durationTotal);
+    });
+  
+    return sum;
+}
+
+function getDay(data){
+    let arr = [];
+    data.map(({ day }) => {
+        const date = new Date(day);
+        console.log(date.getDay());
+        arr.push(days[date.getDay()]);
+    });
+    //console.log(arr);
+    return arr;
+}
+function getWeight(data){
+
+}
+// function getWorkouts(data){
+
+// }
+async function populate(data) {
+    console.log(data);
+  const label = getDay(data);
+  const durations = getDuration(data);
+  const lbs = getWeight(data);
+//   const workouts = getWorkouts(data);
+    console.log(durations);
+  const durationChart = new Chart(firstCtx, {
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+      labels: label,
+      datasets: [
+        {
+          label: 'Workout Duration In Minutes',
+          backgroundColor: 'red',
+          borderColor: 'red',
+          data: durations,
+        },
+      ],
     },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+  });
+  const weightChart = new Chart(secCtx, {
+      type: 'bar',
+      data:{
+          labels: label,
+          datasets: [
+            {
+                label: 'Total Weight Per Workout',
+                backgroundColor: 'red',
+                borderColor: 'red',
+                data: lbs,
+              },
+          ]
+      }
+  })
+}
+
+API.getWorkoutsInRange().then(populate);
